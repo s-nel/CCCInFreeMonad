50e761a0fb600f18801134eb36ab64585e656414
Samuel Nelson <samuel.nelson@elastic.co>
2019-02-21 16:12:17 +1300

    Simplified interface. No need to specify entities up front

--- src/main/scala/uow/UnitOfWorkPoc.scala
+++ src/main/scala/uow/UnitOfWorkPoc.scala
@@ -5,10 +5,20 @@ import cats.implicits._
 import shapeless._
 import shapeless.Nat._
 import shapeless.ops.hlist._
-import uow.UnitOfWorkPoc.Domain.{MultiEntityExample, SingleEntityExample}
-import uow.UnitOfWorkPoc.{DefaultUnitOfWorkExecutor, Instances, ZookeeperBackingStore}
+import uow.UnitOfWorkPoc.Domain.MultiEntityExample
+import uow.UnitOfWorkPoc.{
+  Bar,
+  DefaultUnitOfWorkExecutor,
+  Foo,
+  Repository,
+  UnitOfWorkExecutor,
+  ZookeeperBackingStore,
+  ZookeeperRepository
+}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.reflect.runtime.universe._
 
 object UnitOfWorkPoc {
   trait Entity {
@@ -35,151 +45,100 @@ object UnitOfWorkPoc {
   /**
     * Unit of work abstraction that works on a single entity type
     */
-  trait UnitOfWork[E <: Entity, State] {
-    def get(id: E#Id): StateT[Future, State, Option[E]]
-    def create(e: E): StateT[Future, State, Unit]
-    def update(e: E): StateT[Future, State, Unit]
-    def delete(id: E#Id): StateT[Future, State, Unit]
+  trait UnitOfWork[BS <: AtomicBackingStore, State] {
+    def get[E <: Entity](id: E#Id)(implicit repository: Repository[BS, E],
+                                   typ: TypeTag[E]): StateT[Future, State, Option[E]]
+    def create[E <: Entity](e: E)(implicit repository: Repository[BS, E], typ: TypeTag[E]): StateT[Future, State, Unit]
+    def update[E <: Entity](e: E)(implicit repository: Repository[BS, E], typ: TypeTag[E]): StateT[Future, State, Unit]
+    def delete[E <: Entity](id: E#Id)(implicit repository: Repository[BS, E],
+                                      typ: TypeTag[E]): StateT[Future, State, Unit]
   }
 
   trait BackingStore
   trait AtomicBackingStore extends BackingStore
   trait ZookeeperBackingStore extends AtomicBackingStore
 
+  class EntityStateMapIS[K, V]
+  implicit def entityToState[E <: Entity]: EntityStateMapIS[TypeTag[E], Map[E#Id, Option[E]]] =
+    new EntityStateMapIS[TypeTag[E], Map[E#Id, Option[E]]]
+
   /**
     * State of the unit of work
     * @param ops   The operations that will be committed when the unit of work has finished
     * @param cache The read cache that is kept consistent with the operations performed in this unit of work
     */
-  final case class UnitOfWorkState[BS <: BackingStore, E <: Entity](ops: Seq[BatchOp[BS]], cache: Map[E#Id, Option[E]])
+  final case class UnitOfWorkState[BS <: BackingStore](ops: Seq[BatchOp[BS]], cache: HMap[EntityStateMapIS])
 
-  /**
-    * Object that can execute units ofs works. Forgive my amateur shapeless
-    */
-  trait UnitOfWorkExecutor[BS <: AtomicBackingStore] {
-    // single entity unit of work
-    def execute[E1 <: Entity, T](
-        f: UnitOfWork[E1, UnitOfWorkState[BS, E1] :: HNil] => StateT[Future, UnitOfWorkState[BS, E1] :: HNil, T])(
-        implicit repo1: Repository[BS, E1]): Future[T]
-
-    // two entity unit of work
-    def execute[E1 <: Entity, E2 <: Entity, T](
-        f: (UnitOfWork[E1, UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil],
-            UnitOfWork[E2, UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil]) => StateT[
-          Future,
-          UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil,
-          T])(implicit repo1: Repository[BS, E1],
-              repo2: Repository[BS, E2],
-              at0: At[UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil, Nat._0] {
-                type Out = UnitOfWorkState[BS, E1]
-              },
-              at1: At[UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil, Nat._1] {
-                type Out = UnitOfWorkState[BS, E2]
-              },
-              toList: ToList[Seq[BatchOp[BS]] :: Seq[BatchOp[BS]] :: HNil, Seq[BatchOp[BS]]]): Future[T]
-
-    // we can define more functions that take more types of entities
-  }
-
-  class DefaultUnitOfWorkExecutor[BS <: AtomicBackingStore](implicit executionContext: ExecutionContext)
-      extends UnitOfWorkExecutor[BS] {
-
-    class DefaultUnitOfWork[E <: Entity, States <: HList](
-        fromStates: States => UnitOfWorkState[BS, E],
-        toStates: (States, UnitOfWorkState[BS, E]) => States)(implicit repo: Repository[BS, E])
-        extends UnitOfWork[E, States] {
-      override def get(id: E#Id): StateT[Future, States, Option[E]] =
-        StateT[Future, States, Option[E]] { states =>
-          val state = fromStates(states)
-          (state.cache.get(id) match {
-            case Some(Some(e)) => Future.successful((state, Some(e)))
-            case Some(None)    => Future.successful((state, None))
-            case None =>
-              repo.get(id).map {
-                case Some(e) =>
-                  val newState: UnitOfWorkState[BS, E] =
-                    state.copy[BS, E](cache = state.cache.updated(id, Some(e)))
-                  (newState, Some(e))
-                case None =>
-                  val newState = state.copy[BS, E](cache = state.cache.updated(id, None))
-                  (newState, None)
-              }
-          }).map {
-            case (newState, v) => (toStates(states, newState), v)
-          }
+  class DefaultUnitOfWork[BS <: AtomicBackingStore] extends UnitOfWork[BS, UnitOfWorkState[BS]] {
+    override def get[E <: Entity](id: E#Id)(implicit repo: Repository[BS, E],
+                                            typ: TypeTag[E]): StateT[Future, UnitOfWorkState[BS], Option[E]] =
+      StateT[Future, UnitOfWorkState[BS], Option[E]] { state =>
+        val cache = state.cache.get(typ).getOrElse(Map.empty[E#Id, Option[E]])
+        cache.get(id) match {
+          case Some(Some(e)) => Future.successful((state, Some(e)))
+          case Some(None)    => Future.successful((state, None))
+          case None =>
+            repo.get(id).map {
+              case Some(e) =>
+                val updatedCache = cache.updated(id, Some(e))
+                val newState = state.copy(cache = state.cache + (typ, updatedCache))
+                (newState, Some(e))
+              case None =>
+                val updatedCache = cache.updated(id, None)
+                val newState = state.copy(cache = state.cache + (typ, updatedCache))
+                (newState, None)
+            }
         }
 
-      override def create(e: E): StateT[Future, States, Unit] =
-        StateT[Future, States, Unit] { states =>
-          val state = fromStates(states)
-          (state.cache.get(e.id) match {
-            case Some(Some(e)) =>
-              Future.failed(new Exception("Entity already exists"))
-            case _ =>
-              val newState =
-                state.copy(ops = state.ops ++ Seq(CreateOp[BS, E](e)), cache = state.cache.updated(e.id, Some(e)))
-              Future.successful((newState, ()))
-          }).map {
-            case (newState, v) => (toStates(states, newState), v)
-          }
-        }
-
-      override def update(e: E): StateT[Future, States, Unit] =
-        StateT[Future, States, Unit] { states =>
-          val state = fromStates(states)
-          val newState =
-            state.copy(ops = state.ops ++ Seq(UpdateOp[BS, E](e)), cache = state.cache.updated(e.id, Some(e)))
-          Future.successful((newState, ())).map {
-            case (newState, v) => (toStates(states, newState), v)
-          }
-        }
-
-      override def delete(id: E#Id): StateT[Future, States, Unit] = ???
-    }
-
-    override def execute[E1 <: Entity, T](
-        f: UnitOfWork[E1, UnitOfWorkState[BS, E1] :: HNil] => StateT[Future, UnitOfWorkState[BS, E1] :: HNil, T])(
-        implicit repo1: Repository[BS, E1]): Future[T] = ???
-
-    override def execute[E1 <: Entity, E2 <: Entity, T](
-        f: (UnitOfWork[E1, UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil],
-            UnitOfWork[E2, UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil]) => StateT[
-          Future,
-          UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil,
-          T])(implicit repo1: Repository[BS, E1],
-              repo2: Repository[BS, E2],
-              at0: At[UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil, Nat._0] {
-                type Out = UnitOfWorkState[BS, E1]
-              },
-              at1: At[UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil, Nat._1] {
-                type Out = UnitOfWorkState[BS, E2]
-              },
-              toList: ToList[Seq[BatchOp[BS]] :: Seq[BatchOp[BS]] :: HNil, Seq[BatchOp[BS]]]): Future[T] = {
-      type States = UnitOfWorkState[BS, E1] :: UnitOfWorkState[BS, E2] :: HNil
-      val unitOfWork1: UnitOfWork[E1, States] = new DefaultUnitOfWork[E1, States](at0.apply, {
-        (states: States, newState: UnitOfWorkState[BS, E1]) =>
-          states.updatedAt(_0, newState)
-      })
-      val unitOfWork2: UnitOfWork[E2, States] = new DefaultUnitOfWork[E2, States](at1.apply, {
-        (states: States, newState: UnitOfWorkState[BS, E2]) =>
-          states.updatedAt(_1, newState)
-      })
-
-      object toOps extends Poly1 {
-        implicit def unitOfWorkStateCase[E <: Entity]: Case.Aux[UnitOfWorkState[BS, E], Seq[BatchOp[BS]]] = at(_.ops)
       }
 
+    override def create[E <: Entity](e: E)(implicit repository: Repository[BS, E],
+                                           typ: TypeTag[E]): StateT[Future, UnitOfWorkState[BS], Unit] =
+      StateT[Future, UnitOfWorkState[BS], Unit] { state =>
+        val cache = state.cache.get(typ).getOrElse(Map.empty[E#Id, Option[E]])
+        cache.get(e.id) match {
+          case Some(Some(e)) =>
+            Future.failed(new Exception("Entity already exists"))
+          case _ =>
+            val id: E#Id = e.id
+            val updatedCache = cache.updated(id, Some(e))
+            val newState =
+              state.copy(ops = state.ops ++ Seq(CreateOp[BS, E](e)), cache = state.cache + (typ, updatedCache))
+            Future.successful((newState, ()))
+        }
+      }
+
+    override def update[E <: Entity](e: E)(implicit repository: Repository[BS, E],
+                                           typ: TypeTag[E]): StateT[Future, UnitOfWorkState[BS], Unit] =
+      StateT[Future, UnitOfWorkState[BS], Unit] { state =>
+        val cache = state.cache.get(typ).getOrElse(Map.empty[E#Id, Option[E]])
+        val updatedCache = cache.updated(e.id, Some(e))
+        val newState =
+          state.copy(ops = state.ops ++ Seq(UpdateOp[BS, E](e)), cache = state.cache + (typ, updatedCache))
+        Future.successful((newState, ()))
+      }
+
+    override def delete[E <: Entity](id: E#Id)(implicit repository: Repository[BS, E],
+                                               typ: TypeTag[E]): StateT[Future, UnitOfWorkState[BS], Unit] = ???
+  }
+
+  trait UnitOfWorkExecutor[BS <: AtomicBackingStore] {
+    def execute[T](f: UnitOfWork[BS, UnitOfWorkState[BS]] => StateT[Future, UnitOfWorkState[BS], T]): Future[T]
+  }
+
+  class DefaultUnitOfWorkExecutor[BS <: AtomicBackingStore, E <: Entity](implicit repo: Repository[BS, E])
+      extends UnitOfWorkExecutor[BS] {
+    override def execute[T](
+        f: UnitOfWork[BS, UnitOfWorkState[BS]] => StateT[Future, UnitOfWorkState[BS], T]): Future[T] = {
       for {
-        (finalState, result) <- f(unitOfWork1, unitOfWork2).run(
-          UnitOfWorkState[BS, E1](Seq.empty, Map.empty) :: UnitOfWorkState[BS, E2](Seq.empty, Map.empty) :: HNil)
-        allOps = finalState.map(toOps).toList[Seq[BatchOp[BS]]].toSeq.flatten
-        _ <- repo1.commit(allOps)
+        (finalState, result) <- f(new DefaultUnitOfWork[BS])
+          .run(UnitOfWorkState(Seq.empty[BatchOp[BS]], HMap.empty[EntityStateMapIS]))
+        _ <- repo.commit(finalState.ops)
       } yield result
     }
   }
 
-  class ZookeeperRepository[E <: Entity](implicit executionContext: ExecutionContext)
-      extends Repository[ZookeeperBackingStore, E] {
+  class ZookeeperRepository[E <: Entity] extends Repository[ZookeeperBackingStore, E] {
     override def get(id: E#Id): Future[Option[E]] = Future {
       println(s"get ${id}")
       None
@@ -191,10 +150,9 @@ object UnitOfWorkPoc {
 
     override def delete(id: E#Id): Future[Unit] = ???
 
-    override def commit(ops: Seq[BatchOp[ZookeeperBackingStore]]): Future[Unit] =
-      Future {
-        println(s"commit ops ${ops}")
-      }
+    override def commit(ops: Seq[BatchOp[ZookeeperBackingStore]]): Future[Unit] = Future {
+      println(s"commit ops ${ops}")
+    }
   }
 
   //
@@ -207,65 +165,44 @@ object UnitOfWorkPoc {
     type Id = Int
   }
 
-  object Instances {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    implicit val fooRepoInstance: Repository[ZookeeperBackingStore, Foo] =
-      new ZookeeperRepository[Foo]
-
-    implicit val barRepoInstance: Repository[ZookeeperBackingStore, Bar] = new ZookeeperRepository[Bar]
-
-    implicit val unitOfWorkExecutorInstance: UnitOfWorkExecutor[ZookeeperBackingStore] =
-      new DefaultUnitOfWorkExecutor[ZookeeperBackingStore]
-  }
-
   object Domain {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    /**
-      * Example usage with a single kind of entity
-      */
-    class SingleEntityExample[BackingStore <: AtomicBackingStore](unitOfWorkExecutor: UnitOfWorkExecutor[BackingStore])(
-        implicit fooRepo: Repository[BackingStore, Foo]) {
-
-      def singleTest: Future[Option[Foo]] = unitOfWorkExecutor.execute[Foo, Option[Foo]] { uow =>
-        for {
-          _ <- uow.create(Foo(1, "foo"))
-          foo <- uow.get(1)
-        } yield foo
-      }
-    }
 
     /**
       * Example usage with multiple kinds of entities
       */
-    class MultiEntityExample[BackingStore <: AtomicBackingStore](implicit fooRepo: Repository[BackingStore, Foo],
-                                                                 barRepo: Repository[BackingStore, Bar],
-                                                                 unitOfWorkExecutor: UnitOfWorkExecutor[BackingStore]) {
-      def multiTest: Future[Unit] = unitOfWorkExecutor.execute[Foo, Bar, Unit] { (foos, bars) =>
+    class MultiEntityExample[BS <: AtomicBackingStore](implicit fooRepo: Repository[BS, Foo],
+                                                       barRepo: Repository[BS, Bar],
+                                                       executor: UnitOfWorkExecutor[BS]) {
+      def multiTest: Future[Option[Foo]] = executor.execute { uow =>
         for {
-          nonExistantFoo <- foos.get(1)  // read from repo
+          nonExistantFoo <- uow.get[Foo](1) // read from repo
           _ = assert(nonExistantFoo == None)
-          _ <- foos.create(Foo(1, "foo"))
-          _ <- bars.create(Bar(1, "bar")) // create a different entity
-          foo1 <- foos.get(1) // read insert from cache
+          nonExistantBar <- uow.get[Bar](1) // read from repo
+          _ <- uow.create(Foo(1, "foo"))
+          _ <- uow.create(Bar(1, "bar")) // create a different entity with same value id
+          foo1 <- uow.get[Foo](1) // read insert from cache
           _ = assert(foo1 == Some(Foo(1, "foo")))
-        } yield ()
+          bar1 <- uow.get[Bar](1) // read other insert from cache same value id
+          _ = assert(bar1 == Some(Bar(1, "bar")))
+        } yield foo1
       } // implicit commit
     }
   }
 }
 
 object App {
-  import Instances._
-  import scala.concurrent.duration._
-  import scala.concurrent.ExecutionContext.Implicits.global
   import scala.concurrent.Await
+  import scala.concurrent.duration._
 
   def main(args: Array[String]): Unit = {
-    Await.result(
-      new MultiEntityExample[ZookeeperBackingStore].multiTest,
-      1.second)
+    implicit val fooRepoInstance: Repository[ZookeeperBackingStore, Foo] =
+      new ZookeeperRepository[Foo]
 
+    implicit val barRepoInstance: Repository[ZookeeperBackingStore, Bar] = new ZookeeperRepository[Bar]
+
+    implicit val unitOfWorkExecutorInstance: UnitOfWorkExecutor[ZookeeperBackingStore] =
+      new DefaultUnitOfWorkExecutor[ZookeeperBackingStore, Foo]
+
+    Await.result(new MultiEntityExample[ZookeeperBackingStore].multiTest, 1.second)
   }
 }
